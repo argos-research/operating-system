d700f0251ab2f1102272c821bb0d67f56ecd09ed
Alexander Weidinger <alexander.weidinger@tum.de>
2017-12-01 14:45:46 +0100

    Improve reusability of the tftp-folder.sh script.

--- config-data/tftp-folder.sh
+++ config-data/tftp-folder.sh
@@ -2,23 +2,12 @@
 #Generates the needed tftp folders.. see dhcpd.conf in /etc/dhcp/dhcpd.conf for folders you might need
 echo "Generating Folders for TFTPD Boot please refer to /etc/dhcp/dhcpd.conf to find corresponding machines"
 
-for i in `seq 1 5;`
+boards=(pandaboard raspberry odroid zedzybo qemuvm)
+
+for board in ${boards[*]}
 do
-mkdir -p /var/lib/tftpboot/pandaboard/$i
-done
-for i in `seq 1 5;`
-do
-mkdir -p /var/lib/tftpboot/raspberry/$i
-done
-for i in `seq 1 5;`
-do
-mkdir -p /var/lib/tftpboot/odroid/$i
-done
-for i in `seq 1 5;`
-do
-mkdir -p /var/lib/tftpboot/zedzybo/$i
-done
-for i in `seq 1 5;`
-do
-mkdir -p /var/lib/tftpboot/qemuvm/$i
+	for i in `seq 1 5;`
+	do
+		mkdir -p /var/lib/tftpboot/$board/$i
+	done
 done
