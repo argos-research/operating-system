61c746b83f483c99e12687667f09266057aee828
BugUser0815 <ga67wel@mytum.de>
2018-06-04 09:40:32 +0200

    Update setup script to checkout 18.02 branch

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
+git checkout 18.02
 git submodule update --init
 make toolchain
 make ports
-make jenkins_build_dir
-make jenkins_run
+make jenkins_build_dir "GENODE_TARGET = focnados_pbxa9"
+make jenkins_run "GENODE_TARGET = focnados_pbxa9"
