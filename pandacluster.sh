f2e137aa73ca0fedfbf5627e2cd434a64183ec93
BugUser0815 <ga67wel@mytum.de>
2018-10-16 17:10:13 +0200

    Update Pandacluster script

--- pandacluster.sh
+++ pandacluster.sh
@@ -9,4 +9,19 @@ scp build/genode-focnados_panda/var/run/dom0-HW/image.elf reisner@supermvs:/var/
 sed -i 's/02:02:00:00:00:03/02:02:00:00:00:04/g' genode-dom0-HW/run/dom0-HW.run
 make jenkins_run
 scp build/genode-focnados_panda/var/run/dom0-HW/image.elf reisner@supermvs:/var/lib/tftpboot/pandaboard/pandacluster/04
-sed -i 's/02:02:00:00:00:04/02:02:00:00:00:01/g' genode-dom0-HW/run/dom0-HW.run
+sed -i 's/02:02:00:00:00:04/02:02:00:00:00:05/g' genode-dom0-HW/run/dom0-HW.run
+make jenkins_run
+scp build/genode-focnados_panda/var/run/dom0-HW/image.elf reisner@supermvs:/var/lib/tftpboot/pandaboard/pandacluster/05
+sed -i 's/02:02:00:00:00:05/02:02:00:00:00:06/g' genode-dom0-HW/run/dom0-HW.run
+make jenkins_run
+scp build/genode-focnados_panda/var/run/dom0-HW/image.elf reisner@supermvs:/var/lib/tftpboot/pandaboard/pandacluster/06
+sed -i 's/02:02:00:00:00:06/02:02:00:00:00:07/g' genode-dom0-HW/run/dom0-HW.run
+make jenkins_run
+scp build/genode-focnados_panda/var/run/dom0-HW/image.elf reisner@supermvs:/var/lib/tftpboot/pandaboard/pandacluster/07
+sed -i 's/02:02:00:00:00:07/02:02:00:00:00:08/g' genode-dom0-HW/run/dom0-HW.run
+make jenkins_run
+scp build/genode-focnados_panda/var/run/dom0-HW/image.elf reisner@supermvs:/var/lib/tftpboot/pandaboard/pandacluster/08
+sed -i 's/02:02:00:00:00:08/02:02:00:00:00:09/g' genode-dom0-HW/run/dom0-HW.run
+make jenkins_run
+scp build/genode-focnados_panda/var/run/dom0-HW/image.elf reisner@supermvs:/var/lib/tftpboot/pandaboard/pandacluster/09
+sed -i 's/02:02:00:00:00:09/02:02:00:00:00:01/g' genode-dom0-HW/run/dom0-HW.run
