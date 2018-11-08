44baaf480ffa230ec1c8ddded17252ef7071720d
BugUser0815 <ga67wel@mytum.de>
2018-11-08 12:47:17 +0100

    Fix setup script and remove debug from TL

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 target="GENODE_TARGET = focnados_pbxa9"
-git checkout 18.02
+echo $target
 git submodule update --init
 make packages
 make toolchain
 make ports
-make jenkins_build_dir $target
-make jenkins_run $target
-make tasks $target
-
+make jenkins_build_dir "$target"
+make tasks "$target"
+make jenkins_run "$target"
